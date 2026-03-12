c5f06f22985f95bdac530e2eaf037490d8fb93b4
Андрей Иванов <andrejivanov@MacBook-Air-Andrej.local>
2026-03-12 09:47:12 +0300

    создание функции получения данных по айпи

--- utils.py
+++ utils.py
@@ -8,4 +8,13 @@ def validate_ip(ip_text):
         return None
 
 
+def get_ip_info(ip_obj):
+    return {
+        "version": ip_obj.version,
+        "is_private": ip_obj.is_private,
+        "is_global": ip_obj.is_global,
+        "is_loopback": ip_obj.is_loopback,
+    }
+
+
 
