89af59ea083950141b59892cf639c3e057b359a8
rMononoke <brawlfrisk8@gmail.com>
2026-03-12 09:40:50 +0300

    update: full destructured code

--- main.py
+++ main.py
@@ -1,31 +1,6 @@
 from colorama import Fore, Style, init
 from config import APP_NAME, APP_VERSION, DEFAULT_IP 
-import ipaddress
-
-def validate_ip(ip_text):
-    try:
-        return ipaddress.ip_address(ip_text)
-    except ValueError:
-        return None
-
-
-def get_ip_info(ip_obj):
-    return {
-        "version": ip_obj.version,
-        "is_private": ip_obj.is_private,
-        "is_global": ip_obj.is_global,
-        "is_loopback": ip_obj.is_loopback,
-    }
-
-
-def format_result(ip_obj, info):
-    return (
-        f"IP address: {ip_obj}\n"
-        f"Version: IPv{info['version']}\n"
-        f"Private: {info['is_private']}\n"
-        f"Global: {info['is_global']}\n"
-        f"Loopback: {info['is_loopback']}"
-    )
+from utils import validate_ip, get_ip_info, format_result
 
 init(autoreset=True)
 
